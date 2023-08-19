f3cc9af6e8bdbcd6a585dcf78841ad0745767e64
wuyax <pengqingkai@gmail.com>
2023-08-19 10:33:43 +0800

    fix:script path

--- index.user.js
+++ index.user.js
@@ -5,9 +5,9 @@
 // @author       yee.wang
 // @include      https://*.iconfont.cn/*
 // @require      http://cdn.bootcss.com/jquery/1.12.4/jquery.min.js
-// @updateURL    https://github.com/yee94/tampermonkey-iconfont-iconify/raw/main/index.user.js
-// @downloadURL  https://github.com/yee94/tampermonkey-iconfont-iconify/raw/main/index.user.js
-// @require  	 https://github.com/yee94/tampermonkey-iconfont-iconify/raw/main/dist/main.js
+// @updateURL    https://github.com/wuyax/tampermonkey-iconfont-iconify/raw/main/index.user.js
+// @downloadURL  https://github.com/wuyax/tampermonkey-iconfont-iconify/raw/main/index.user.js
+// @require  	 https://github.com/wuyax/tampermonkey-iconfont-iconify/raw/main/dist/main.js
 // @grant        unsafeWindow
 // @connect      *
 // ==/UserScript==
